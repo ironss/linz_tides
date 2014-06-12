1ec9dd2f5857003c3f5ea34846468a2fe9154e8d
Stephen Irons <stephen.irons@clear.net.nz>
2014-06-13 10:47:50 +1200

    Attempt to add multiple tide events in one transaction failed.

--- linz_tides.lua
+++ linz_tides.lua
@@ -42,13 +42,27 @@ local events = {}
 local function Event_new(e)
    events[#events+1] = e
 
-   print(e.port, e.timestamp:format('%Y-%m-%d %H:%M:%S', 'UTC'), e.event_type, e.height)
+--   print(e.port, e.timestamp:format('%Y-%m-%d %H:%M:%S', 'UTC'), e.event_type, e.height)
    local result = assert(cx:execute(string.format([[
       INSERT INTO 'primary_tide_events'
       VALUES ("%s", '%s', '%s', '%3.1f')]], e.port, e.timestamp:format('%Y-%m-%d %H:%M:%S', 'UTC'), e.event_type, e.height)
    ))
 
    return e
+end
+
+local function Events_new(event_list)
+   local statement = 'BEGIN TRANSACTION;\n'
+   for _, e in ipairs(event_list) do
+      Event_new(e)
+--      events[#events+1] = e
+--      print(e.port, e.timestamp:format('%Y-%m-%d %H:%M:%S', 'UTC'), e.event_type, e.height)
+      statement = statement .. string.format(
+      'INSERT INTO "primary_tide_events" VALUES ("%s", "%s", "%s", "%3.1f");\n', e.port, e.timestamp:format('%Y-%m-%d %H:%M:%S', 'UTC'), e.event_type, e.height)
+   end
+   statement = statement .. 'COMMIT TRANSACTION;'
+--   print(statement)
+--   local result = assert(cx:execute(statement))
 end
 
 -- Extract tidal data from an open file
@@ -60,8 +74,8 @@ local function read_linz_tide_file(f, events)
    local tz, _ = l3:match('^(.-),(.-)%c*$')
  
 --   print(port, tz, latitude, '"'..longitude..'"', tz)
-   local events = events or {}
    for l in f:lines() do
+      print(l)
       local day, _, month, year, t1, h1, t2, h2, t3, h3, t4, h4 = l:match('^(.-),(.-),(.-),(.-),(.-),(.-),(.-),(.-),(.-),(.-),(.-),(.-)%c*$')
       h1 = tonumber(h1)
       h2 = tonumber(h2)
@@ -69,21 +83,23 @@ local function read_linz_tide_file(f, events)
       h4 = tonumber(h4)
 
       if day ~= nil then
-         events[#events+1] = Event_new{ port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t1:sub(1, 2), min=t1:sub(4, 5), tz=tz}, height=h1, event_type=h1 > h2 and "high" or "low" }
-         events[#events+1] = Event_new{ port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t2:sub(1, 2), min=t2:sub(4, 5), tz=tz}, height=h2, event_type=h2 > h3 and "high" or "low" }
-         events[#events+1] = Event_new{ port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t3:sub(1, 2), min=t3:sub(4, 5), tz=tz}, height=h3, event_type=h3 > h2 and "high" or "low" }
+         local events = {}
+
+         events[#events+1] = { port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t1:sub(1, 2), min=t1:sub(4, 5), tz=tz}, height=h1, event_type=h1 > h2 and "high" or "low" }
+         events[#events+1] = { port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t2:sub(1, 2), min=t2:sub(4, 5), tz=tz}, height=h2, event_type=h2 > h3 and "high" or "low" }
+         events[#events+1] = { port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t3:sub(1, 2), min=t3:sub(4, 5), tz=tz}, height=h3, event_type=h3 > h2 and "high" or "low" }
          if (t4 ~= '') then
    --         print (port, year, month, day, tz, t4, h4)
-            events[#events+1] = Event_new{ port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t4:sub(1, 2), min=t4:sub(4, 5), tz=tz}, height=h4, event_type=h4 > h3 and "high" or "low" }
+            events[#events+1] = { port=port, timestamp=datetime.new{year=year, month=month, day=day, hour=t4:sub(1, 2), min=t4:sub(4, 5), tz=tz}, height=h4, event_type=h4 > h3 and "high" or "low" }
          end
+         
+         Events_new(events)
       end
    end
    return events
 end
 
 local function read_linz_tide_filename(filename, events)
---   erase_tides()
---   create_tides()
    local f = io.open(filename)
    local events = read_linz_tide_file(f, events)
    return events
