f1330b5689390ebaa79cafa7945d173779ec75f3
Stephen Irons <stephen.irons@clear.net.nz>
2014-06-15 20:39:03 +1200

    Must create trips after tides (ie. last) and delete trips first.

--- create-db.lua
+++ create-db.lua
@@ -15,18 +15,18 @@ local ports = require 'linz_ports'
 local tides = require 'linz_tides'
 local trips = require 'trips'
 
---tides.erase_tables()
 --trips.erase_tables()
+--tides.erase_tables()
 --ports.erase_tables()
 
 ports.create_tables()
 ports.populate_tables('secondaryports2013-14.csv')
 
-trips.create_tables()
-trips.populate_tables()
-
 tides.create_tables()
 for _, fn in ipairs(tides_filenames) do
    tides.populate_tables(fn)
 end
+
+trips.create_tables()
+trips.populate_tables()
 
