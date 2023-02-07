3be946bb02435a088bafea286069a7746eeec251
Stephen Irons <ironss@users.noreply.github.com>
2023-02-07 22:06:36 +1300

    Tidied up interpolation function.

--- tides_interpolate.py
+++ tides_interpolate.py
@@ -5,34 +5,34 @@ import math
 def tide_interpolate(db, port, time_ts):
     con = sqlite3.connect(db)
     res = con.execute("""
-        WITH 
+        WITH
             t1 as (
-                SELECT * 
-                FROM tides 
-                WHERE port = :port AND timestamp >= :ts 
-                ORDER BY datetime ASC 
+                SELECT timestamp, height
+                FROM tides
+                WHERE port = :port AND timestamp >= :ts
+                ORDER BY timestamp ASC
                 LIMIT 1
             ),
             t2 as (
-                SELECT * 
-                FROM tides 
+                SELECT timestamp, height
+                FROM tides
                 WHERE port = :port AND timestamp < :ts
-                ORDER BY datetime DESC 
+                ORDER BY timestamp DESC
                 LIMIT 1
             )
-            
+
         SELECT * from t1
         UNION
-        SELECT * from t2;    
+        SELECT * from t2;
     """, {'port':port, 'ts':time_ts})
 
     rows = res.fetchall()
-    _, dt_str_prev, ts_prev, height_prev = rows[0]
-    _, dt_str_next, ts_next, height_next = rows[1]
+    ts_prev, height_prev = rows[0]
+    ts_next, height_next = rows[1]
 
     phase = (time_ts - ts_prev) / (ts_next - ts_prev) * math.pi
     height = (1 + math.cos(phase))/2 * (height_prev - height_next) + height_next
-    
+
     return port, time_ts, height
 
 
@@ -43,23 +43,21 @@ if __name__ == '__main__':
     import dateparser
     parse_date = dateparser.parse
 
-    
+
     parser = argparse.ArgumentParser(
         description="Calculate tide height at any time."
     )
-    
-    parser.add_argument('-d', '--database', default='linz_tides.db')
+    parser.add_argument('-d', '--database', default='tides_db.sqlite3')
     parser.add_argument('-t', '--time', action='append')
     parser.add_argument('port', nargs='+')
     args = parser.parse_args()
-    #print(args)
 
     if not args.time:
         args.time = ['now']
-    
+
     ports = args.port
     db_name = args.database
-    
+
     timestamps_dt = []
     for dt_str in args.time:
         try:
@@ -75,4 +73,3 @@ if __name__ == '__main__':
         for dt in timestamps_dt:
             _, _, height = tide_interpolate(db_name, port, dt.timestamp())
             print(port, dt, height)
-
